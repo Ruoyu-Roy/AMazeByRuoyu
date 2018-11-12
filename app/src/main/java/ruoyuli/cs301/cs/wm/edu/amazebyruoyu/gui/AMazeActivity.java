1e43cba5f372e91555020e70a58d1e57b4b258a5
ruoyuli <rli08@email.wm.edu>
2018-11-12 13:04:44 -0500

    2nd upload: xml file generating

--- app/src/main/java/ruoyuli/cs301/cs/wm/edu/amazebyruoyu/gui/AMazeActivity.java
+++ app/src/main/java/ruoyuli/cs301/cs/wm/edu/amazebyruoyu/gui/AMazeActivity.java
@@ -7,16 +7,20 @@ import android.widget.ArrayAdapter;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.Spinner;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.TextView;
+import android.view.View;
+import android.content.Intent;
+import android.media.MediaPlayer;
+
+import java.io.File;
 
 import java.util.*;
 
 import ruoyuli.cs301.cs.wm.edu.amazebyruoyu.R;
 
 /**
- *
- *
  * @author ruoyuli
  */
 
@@ -26,50 +30,57 @@ public class AMazeActivity extends AppCompatActivity {
     private TextView difLevel;
     private Spinner gen_spinner;
     private Spinner driver_spinner;
+    private MediaPlayer mediaPlayer;
 
     protected String DFS = "DFS",
-                     Prim = "Prim's",
-                     Eller = "Eller's",
-                     Manual = "Manual",
-                     WallF = "WallFollower",
-                     Wiz = "Wizard",
-                     Expl = "Explorer",
-                     Pledge = "Pledge";
+            Prim = "Prim's",
+            Eller = "Eller's",
+            Manual = "Manual",
+            WallF = "WallFollower",
+            Wiz = "Wizard",
+            Expl = "Explorer",
+            Pledge = "Pledge";
+    protected String LOG_V = "AMazeActivity";
 
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.maze_activity);
         initializeVariable();
         setOnSeekBarListener();
         spinnerSet();
+        mediaPlayer = MediaPlayer.create(this, R.raw.title_music);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
     }
 
-    public void initializeVariable(){
+    public void initializeVariable() {
         seekBar = (SeekBar) findViewById(R.id.seekBar1);
         difLevel = (TextView) findViewById(R.id.difLevel);
         gen_spinner = (Spinner) findViewById(R.id.gen_spinner);
         driver_spinner = (Spinner) findViewById(R.id.drv_spinner);
     }
 
-    public void setOnSeekBarListener(){
+    public void setOnSeekBarListener() {
         seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 difLevel.setText("Current Level:" + Integer.toString(progress) + "/15");
-                Log.v("AMazeActivity", "Current level: "+progress);
+                Log.v(LOG_V, "Current level: " + progress);
             }
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
 
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
         });
     }
 
-    private void spinnerSet(){
+    private void spinnerSet() {
         List<String> gen_algo = new ArrayList<String>();
         gen_algo.add(DFS);
         gen_algo.add(Prim);
@@ -87,4 +98,45 @@ public class AMazeActivity extends AppCompatActivity {
         adapter1.setDropDownViewResource(R.layout.spinner_item);
         driver_spinner.setAdapter(adapter1);
     }
+
+    private void addOnSpinnerListener() {
+        gen_spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.v(LOG_V, "Generation Algorithm Selected: " + gen_spinner.getSelectedItem().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        driver_spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.v(LOG_V, "Generation Algorithm Selected: " + driver_spinner.getSelectedItem().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
+    public void generateNewMaze(View view) {
+        Log.v(LOG_V, "New Maze Button Clicked");
+
+        int skillLevel = seekBar.getProgress();
+        String genAlgorithm = gen_spinner.getSelectedItem().toString();
+        String drvAlgorithm = driver_spinner.getSelectedItem().toString();
+        Intent intent = new Intent(this, GeneratingActivity.class);
+
+        intent.putExtra("newMaze", true);
+        intent.putExtra("skillLevel", skillLevel);
+        intent.putExtra("generateAlgorithm", genAlgorithm);
+        intent.putExtra("drvierAlgorithm", drvAlgorithm);
+
+        startActivity(intent);
+    }
+
+
 }
--- app/src/main/java/ruoyuli/cs301/cs/wm/edu/amazebyruoyu/gui/GeneratingActivity.java
+++ app/src/main/java/ruoyuli/cs301/cs/wm/edu/amazebyruoyu/gui/GeneratingActivity.java
@@ -1,4 +1,15 @@
 package ruoyuli.cs301.cs.wm.edu.amazebyruoyu.gui;
 
-public class GeneratingActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import ruoyuli.cs301.cs.wm.edu.amazebyruoyu.R;
+
+public class GeneratingActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.generating_activity);
+    }
 }
